4cc5ed6fd96c36b48616cbd0640cf37634859e41
Jared Carey <itspandorux@gmail.com>
2018-03-01 12:33:56 +1000

    IGB100 Prac 4 Complete

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnRate = 2.0f;
+    private float spawnTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SpawnEnemy();
+	}
+
+    private void SpawnEnemy()
+    {
+        if (Time.time > spawnTimer) {
+            Instantiate(enemy, transform.position, transform.rotation);
+            spawnTimer = Time.time + spawnRate;
+        }
+    }
+}
